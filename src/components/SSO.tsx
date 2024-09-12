c71a1df9ef37f561720467094c7a82adc5d8edf5
Lydia Gorham <lydia@stytch.com>
2024-09-12 11:53:09 -0700

    Add Role Copy for Settings & SSO

--- src/components/SSO.tsx
+++ src/components/SSO.tsx
@@ -6,8 +6,29 @@ const SSO = () => {
     fontFamily: "Courier New",
   };
 
+  const config = {
+    getRoleDescription: (role: any) => {
+      if (role.role_id == 'stytch_admin') {
+          return 'Able to manage settings and members'
+      } else if (role.role_id == 'stytch_member') {
+          return 'Able to view settings and members, but cannot edit'
+      } else {
+          return 'Your custom role description here!'
+      }
+    },
+    getRoleDisplayName: (role: any) => {
+        if (role.role_id == 'stytch_admin') {
+            return 'Admin'
+        } else if (role.role_id == 'stytch_member') {
+            return 'Member'
+        } else {
+            return role.role_id
+        }
+    }
+  }
+
   return (
-      <AdminPortalSSO styles={styles} />
+      <AdminPortalSSO config={config} styles={styles} />
   );
 };
 
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -5,7 +5,27 @@ const Settings = () => {
   const styles = {
     fontFamily: "Courier New",
   };
-  const config = {allowedAuthMethods: [AdminPortalB2BProducts.emailMagicLinks, AdminPortalB2BProducts.sso]};
+  const config = {
+    allowedAuthMethods: [AdminPortalB2BProducts.emailMagicLinks, AdminPortalB2BProducts.sso],
+    getRoleDescription: (role: any) => {
+      if (role.role_id == 'stytch_admin') {
+          return 'Able to manage settings and members'
+      } else if (role.role_id == 'stytch_member') {
+          return 'Able to view settings and members, but cannot edit'
+      } else {
+          return 'Your custom role description here!'
+      }
+    },
+    getRoleDisplayName: (role: any) => {
+        if (role.role_id == 'stytch_admin') {
+            return 'Admin'
+        } else if (role.role_id == 'stytch_member') {
+            return 'Member'
+        } else {
+            return role.role_id
+        }
+    }
+  };
 
   return (
       <AdminPortalOrgSettings config={config} styles={styles} />
