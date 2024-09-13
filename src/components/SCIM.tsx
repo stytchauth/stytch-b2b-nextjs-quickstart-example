a343474a42ee8e0de4e397fb53452e848800f765
Josh Bolduc <jbolduc@stytch.com>
2024-09-13 10:11:19 -0400

    Use shared admin portal config

--- src/components/SCIM.tsx
+++ src/components/SCIM.tsx
@@ -1,13 +1,10 @@
 import { AdminPortalSCIM } from '@stytch/nextjs/b2b/adminPortal'
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { styles } from '@/lib/adminPortalConfig';
 
 const SCIM = () => {
-  const styles = {
-    fontFamily: "Courier New",
-  };
-
   return (
-      <AdminPortalSCIM styles={styles} />
+    <AdminPortalSCIM styles={styles} />
   );
 };
 
